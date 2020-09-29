993967ae5ea03b81b1f5dbd129a0effef8a99e6d
nirajlimbu7@gmail.com <nirajlimbu7@gmail.com>
2020-09-29 22:48:11 +1000

    Update Marketplace.dart
    updated Marketplace

---
+++ lib/Marketplace.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Marketplace extends StatefulWidget {
+  final String title;
+  Marketplace({this.title});
+  _MarketplaceState createState() => _MarketplaceState();
+}
+
+class _MarketplaceState extends State<Marketplace> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(widget.title),
+      ),
+    );
+  }
+}
