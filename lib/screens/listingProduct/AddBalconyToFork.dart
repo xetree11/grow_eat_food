ed8a3b883555015bb0c27f649b93f6622d9d8658
nirajlimbu7@gmail.com <nirajlimbu7@gmail.com>
2020-10-18 20:24:46 +1100

    changes on balconytofork by niraj

--- lib/screens/listingProduct/AddBalconyToFork.dart
+++ lib/screens/listingProduct/AddBalconyToFork.dart
@@ -11,7 +11,37 @@ class _AddBalconyToForkState extends State<AddBalconyToFork> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Comming Soon! '),
+      appBar: AppBar(
+        title: Text('Balcony To Fork'),
+      ),
+      body: Card(
+        elevation: 5,
+        child: Container(
+          padding: EdgeInsets.all(10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: <Widget>[
+              TextField(
+                decoration: InputDecoration(labelText: 'Title'),
+
+                // onChanged: (val) {
+                //   titleInput = val;
+                // },
+              ),
+              TextField(
+                decoration: InputDecoration(labelText: 'Price'),
+
+                // onChanged: (val) => amountInput = val,
+              ),
+              FlatButton(
+                child: Text('Submit'),
+                textColor: Colors.green,
+                onPressed: () {},
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
