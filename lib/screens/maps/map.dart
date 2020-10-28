da76e9e3528df7f6dd09045e581f6ec810987283
nirajlimbu7@gmail.com <nirajlimbu7@gmail.com>
2020-10-29 01:56:59 +1100

    connected with firebase
    fix connection with firebase

---
+++ lib/screens/maps/map.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class GrowEatFood extends StatefulWidget {
+  @override
+  _GrowEatFoodState createState() => _GrowEatFoodState();
+}
+
+class _GrowEatFoodState extends State<GrowEatFood> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
